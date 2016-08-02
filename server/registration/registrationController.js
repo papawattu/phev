28e291dfbdb7f4728e124c75eb53b36aa44bc943
Jamie Nuttall <jamie@wattu.com>
2016-08-02 21:09:05 +0100

    Registration test failure

--- server/registration/registrationController.js
+++ server/registration/registrationController.js
@@ -1,27 +1,6 @@
 var express = require('express')
     , router = express.Router();
 var registrationService = require('./registrationService');
-var winston = require('winston');
-
-winston.emitErrs = true;
-var logger = new winston.Logger({
-    transports: [
-        new winston.transports.Console({
-            timestamp: true,
-            level: 'debug',
-            handleExceptions: true,
-            json: false,
-            colorize: true
-        })
-    ],
-    exitOnError: false
-});
-
-logger.stream = {
-    write: function(message, encoding){
-        logger.debug(message.replace(/\n$/, ''));
-    }
-};
 
 router.post('/', function(req, res) {
   if(registrationService.createUser(req.body.username, req.body.firstname, req.body.lastname,

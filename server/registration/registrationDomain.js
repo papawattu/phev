7297f36da11ba120461b155a9b0c80792bff7d7a
Jamie Nuttall <jamie@wattu.com>
2016-11-17 19:25:33 +0000

    Removed redunand, code moved logs

--- server/registration/registrationDomain.js
+++
@@ -1,9 +0,0 @@
-var user = function(username,firstname,lastname,email,password) {
-            username    = username;
-            firstname   = firstname;
-            lastname    = lastname;
-            email       = email;
-            password    = password;
-        };
-
-module.exports = user;

0c33c36c7ee7f1e6fd074532e88447fbba031f63
Jamie Nuttall <jamie@wattu.com>
2016-12-03 21:39:24 +0000

    removed log file delete code

--- src/common/util.js
+++ src/common/util.js
@@ -1,9 +1,6 @@
 'use strict';
 const logger = require('./logging');
-const findRemoveSync = require('find-remove');
 const EOL = '\r\n';
-
-findRemoveSync('/logs', {extensions: ['.bak', '.log']});
 
 exports.eol = (str) => (str || '') + EOL;
 exports.logger = logger;

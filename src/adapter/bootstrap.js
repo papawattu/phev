6a7746a8ff48d9834c9b9781a868c48cfc764604
Jamie Nuttall <jamie@wattu.com>
2017-01-10 14:37:42 +0000

    added proxy require

--- src/adapter/bootstrap.js
+++ src/adapter/bootstrap.js
@@ -4,7 +4,7 @@ var Adapter = require('adapter');
 Serial1.setup(115200,{path: '/dev/ttyAMA0'});
 
 var adapter = Adapter({sim900: SIM900,
-                           serial: Serial1, 
-                           apn: 'everywhere',
-                           username: 'eesecure',
-                           password: 'secure'});
+	serial: Serial1, 
+	apn: 'everywhere',
+	username: 'eesecure',
+	password: 'secure'});

d080b0eafd28f5b5093d0140caefdcd7fc92323b
Jamie Nuttall <jamie@wattu.com>
2017-04-11 16:34:49 +0100

    mtqq changes

---
+++ src/vehicle_gateway/vehicle_mqtt_gateway.js
@@ -0,0 +1,124 @@
+import net from 'net';
+import uuid from 'uuid';
+import { MessageTypes, MessageCommands, Message } from '../common/message_bus/message_bus';
+import { Topics } from '../common/message_bus/topics';
+import HttpService from '../common/http_service';
+import BaseClass from '../common/base_class';
+import Store from '../common/store/new_store_sync';
+
+const CRLF = '\r\n';
+
+class VehicleSession {
+	constructor() {
+		//super({ name: 'VehicleSession' });
+		this.id = uuid();
+		this.dongleId = null;
+		this.connected = false;
+		this.wifiConnected = false;
+		this.ready = false;
+	}
+}
+
+export default class VehicleGateway extends HttpService {
+	constructor({ messageBus, name = 'VehicleGateway', port = 3081, gatewayPort = 1974, store = new Store() }) {
+		super({ messageBus, name, port });
+		this.server = null;
+		this.gatewayPort = gatewayPort;
+		this.store = store;
+		this.server = null;
+		this.socketTable = [];
+	}
+	handle(cmd,cb) {
+		this.messageBus.sendAndReceiveMessage({ topic: Topics.VEHICLE_HANDLER_TOPIC, payload: cmd, command: MessageCommands.NoOperation,producer: `handle ${cmd.command}`}, (msg) => {
+			cb(msg);
+		});
+	}
+	handleNewConnection(socket) {
+
+		this.socketTable.push(socket);
+		
+		const session = new VehicleSession();
+
+		socket.on('data', (data) => {
+			const cmdLine = data.toString().split(/[, \t\r\n]+/);	
+			this.handle({ id: session.id, command: cmdLine[0], args: cmdLine.slice(1) }, (msg) => {
+				socket.write(msg.payload + CRLF);
+			});
+		});
+
+		this.store.set(session.id, session);
+		
+		this.logger.debug('Vehicle gateway - created new session ' + session.id);
+		socket.write('HELLO PHEV' + CRLF);
+		
+		return session;
+	}
+	getSession(id) {
+		return this.store.get(id);
+	}
+	setSession(session) {
+		return this.store.set(session.id,session);
+	}
+	validateSession(data) {
+		const session = this.store.get(data.sessionId);
+		
+		if(!session) {
+			this.logger.error('Session cannot be validated as not found : ' + data.getSessionId);
+			throw new Error('Session cannot be validated as not found : ' + data.getSessionId);
+		}
+		this.logger.debug('validateSession - Data : ' + JSON.stringify(data));
+		
+		session.connected = true;
+		session.dongleId = data.dongleId;
+		
+		this.store.set(data.sessionId,session);
+		this.logger.debug('validateSession - Stored Session : ' + JSON.stringify(session));
+		return session;
+	}
+	start(done) {
+		super.start(() => {
+			this.registerMessageHandler(Topics.GATEWAY_TOPIC, null, { type: MessageTypes.Request },
+				[{
+					name: MessageCommands.Get,
+					numArgs: 1,
+					handle: this.getSession,
+					async: false,
+				}, {
+					name: 'validateSession',
+					numArgs: 1,
+					handle: this.validateSession,
+					async: false,
+				},{
+					name: MessageCommands.Add,
+					numArgs: 1,
+					handle: this.setSession,
+					async: false,
+				}]);
+
+			if(this.server === null) {
+				this.server = net.createServer(this.handleNewConnection.bind(this));
+			}
+			this.server.on('error', (err) => {
+				throw err;
+			});
+
+			this.server.listen(this.gatewayPort, '0.0.0.0', (err) => {
+				if (err) {
+					throw err;
+				} else {
+					this.logger.info('Vehicle Manager listening on port ' + this.gatewayPort);
+					done();
+				}
+			});
+			this.socketTable = [];
+		});
+
+	}
+	stop(done) {
+		this.server.close((err) => {
+			super.stop(() => {
+				done(err);
+			});
+		});
+	}
+}
---
+++ src/vehicle_gateway/vehicle_mqtt_gateway.spec.js
@@ -0,0 +1,79 @@
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import sinon from 'sinon';
+
+import { ServiceStatus } from '../common/base_service';
+import { Mocks } from '../common/test/mocks';
+import VehicleMqttGateway from './vehicle_mqtt_gateway';
+
+const mqttClient = {};
+const messageBus = Mocks.messageBus;
+const store = Mocks.store;
+const assert = chai.use(chaiAsPromised).assert;
+const sut = new VehicleMqttGateway({ name: 'default', messageBus, store: store });
+
+/*Mocks = {};
+Mocks.httpService = {};
+Mocks.httpService.start = sinon.stub().yields();
+Mocks.httpService.stop = sinon.stub().yields();
+*/
+
+const socket = {};
+socket.on = sinon.stub();
+socket.write = sinon.stub();
+		
+describe('Vehicle Mqtt Gateway', () => {
+	it('Should start', (done) => {
+		sut.server = {} ;
+		sut.server.on = sinon.stub().yields();
+		sut.server.listen = sinon.stub().yields();
+		sut.server.close = sinon.stub().yields();
+		
+		sut.start(() => {
+			assert.equal(sut.status, ServiceStatus.Started);
+			done();
+		});
+	});
+	it('Should accept connections', () => {
+		assert(sut.server.listen.calledOnce,'server listen should be callled once');
+	});
+	it('Should receive data', () => {
+		assert(sut.server.on.calledOnce);
+	});
+	it('Should create new session', () => {
+		sut.handleNewConnection(socket);
+		assert(sut.store.set.calledOnce);
+	});
+	it('Should write HELLO PHEV', () => {
+		assert(socket.write.calledOnce);
+		assert(socket.write.calledWith('HELLO PHEV\r\n'));
+	});
+	it('Should send message', (done)=> {
+		sut.handle({},() => {	
+
+			done();	
+		});
+		assert(messageBus.sendMessage.calledOnce);
+		assert(messageBus.receiveMessageFilter.calledOnce);
+	});
+	it('Should get session', ()=> {
+		Mocks.store.get.returns({});
+		assert.deepEqual(sut.getSession('1234'),{});
+	});
+	it('Should validate session', ()=> {
+		Mocks.store.get.returns({connected: false, id: 'abcdef'});
+		sut.validateSession({sessionId: 'abcdef',dongleId: '12345'});
+		assert.deepEqual(sut.getSession('abcdef'),{id: 'abcdef',connected: true,dongleId: '12345'});
+	});
+	it('Should stop', (done) => {
+		sut.server = {} ;
+		sut.server.close = sinon.stub().yields();
+		
+		sut.stop(() => {
+			assert.equal(sut.status, ServiceStatus.Stopped);
+			assert(sut.server.close.calledOnce);
+			
+			done();
+		});
+	});
+});
